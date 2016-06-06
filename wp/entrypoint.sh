5df9ecde8d3c24e5df11fc674c4d6bb64860689d
SugendhKGanga <SugendhKGanga@users.noreply.github.com>
2016-06-06 14:39:07 +0530

    Update entrypoint.sh

--- wp/entrypoint.sh
+++ wp/entrypoint.sh
@@ -11,19 +11,19 @@ apt-get install -y apache2.2-common
 a2enmod rewrite expires
 service apache2 restart
 apt-get update && apt-get install -y libpng12-dev libjpeg-dev && rm -rf /var/lib/apt/lists/* && docker-php-ext-configure gd --with-png-dir=/usr --with-jpeg-dir=/usr && docker-php-ext-install gd mysqli opcache
-cd /opt/
+#cd /opt/
 
 apt-get update
 apt-get install -y wget
-wget http://wordpress.org/latest.tar.gz
-tar xzvf latest.tar.gz
+#wget http://wordpress.org/latest.tar.gz
+#tar xzvf latest.tar.gz
 apt-get update
 apt-get -y install php5-gd libssh2-php
 apt-get install -y nano
-ls
-cd wordpress/
-ls
-cp wp-config-sample.php wp-config.php
+#ls
+#cd wordpress/
+#ls
+#cp wp-config-sample.php wp-config.php
 
 service apache2 restart
 apt-get install -y rsync
