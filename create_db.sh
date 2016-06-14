18127fb3b55d53abc532d50e584348f9a3d24c62
SugendhKGanga <SugendhKGanga@users.noreply.github.com>
2016-06-14 11:55:56 +0530

    Update create_db.sh

--- create_db.sh
+++ create_db.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 service mysql start
-mysql -e "SET PASSWORD FOR root@'localhost' = PASSWORD('password')";
+#mysql -e "SET PASSWORD FOR root@'localhost' = PASSWORD('password')";
 
 #Specify MySQL User
-export MYSQL_USER=root
+#export MYSQL_USER=root
 #Specify MySQL Password
-export MYSQL_PASSWORD=password
+#export MYSQL_PASSWORD=password
 
-mysql -u $MYSQL_USER -p$MYSQL_PASSWORD -e "create database wp";
-mysql -u $MYSQL_USER -p$MYSQL_PASSWORD -e "FLUSH PRIVILEGES";
+#mysql -u $MYSQL_USER -p$MYSQL_PASSWORD -e "create database wp";
+#mysql -u $MYSQL_USER -p$MYSQL_PASSWORD -e "FLUSH PRIVILEGES";
 
 #mysql  -e "create database wp";
 #mysql -e "FLUSH PRIVILEGES";
+mysql -uroot -e "create database test1234;"
